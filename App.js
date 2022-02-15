4eacc3a3ca7ce1d1b1e77e754ff5236f242c9426
Juann Pablo Brandão Santos <62782269+juannBrandao@users.noreply.github.com>
2022-02-15 15:27:38 -0300

    initial comit

--- App.js
+++ App.js
@@ -1,13 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator, Image } from 'react-native';
+const baseURL = 'https://api.github.com'
+const perPage = 20
 export default function App() {
+const [repositories, setRepositories] = useState([])
+const [loading, setLoading] = useState(false)
+const [page, setPage] = useState(1)
+
+useEffect(()=>{
+  loadApi()
+},[])
+
+async function loadApi(){
+  if(loading) return;
+
+  setLoading(true);
+
+  const response = await axios.get(`${baseURL}/search/repositories?q=react&per_page=${perPage}&page=${page}`)
+  setRepositories([...repositories, ...response.data.items])
+  setPage(page + 1);
+  setLoading(false)
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList 
+      style={{marginTop:35}}
+      contentContainerStyle={{marginHorizontal:20}}
+      data={repositories}
+      keyExtractor={item => String(item.id)}
+      renderItem={({item}) => <ListItem data={item}/> }
+      onEndReached={loadApi}
+      onEndReachedThreshold={0.1}
+      ListFooterComponent={<FootherList load={loading}/>}
+      />
     </View>
   );
+}
+
+function ListItem({data}){
+  return(
+    <View style={styles.listItem}>
+      <Image
+        style={styles.tinyLogo}
+        source={{
+          uri: data.owner.avatar_url,
+        }}
+      />
+
+      <Text style={styles.listText}>
+        {data.full_name}
+      </Text>
+    </View>
+  )
+}
+
+function FootherList({load}){
+  if(!load) return null;
+  return(
+    <View style={styles.loading}>
+        <ActivityIndicator size={25} color="#107AB0"/>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +71,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  listItem:{
+    backgroundColor:'#107AB0',
+    padding: 25,
+    marginTop: 20,
+    borderRadius:10,
+    width: 300,
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent:'space-between'
+    
+  },
+  listText:{
+    fontSize:16,
+    color: '#FFF',
+  },
+  loading:{
+    padding: 15,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+    borderRadius:50,
   },
 });
